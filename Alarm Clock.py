a81885ac0d45453170d91d170e8d6cc3302e5ea7
Muhammad Umair Yaqub <umairyaqubofficial@gmail.com>
2023-03-05 20:05:39 +0500

    Update Alarm Clock.py

--- Alarm Clock.py
+++ Alarm Clock.py
@@ -1,21 +1,46 @@
+# Designed and Developed by Muhammad Umair Yaqub.
+# Made in P A K I S T A N
+
+# Import necessary modules
 from datetime import datetime   
 from playsound import playsound
-alarm_time = input("Enter the time of alarm to be set:HH:MM:SS\n")
-alarm_hour=alarm_time[0:2]
-alarm_minute=alarm_time[3:5]
-alarm_seconds=alarm_time[6:8]
+
+# Get the time of the alarm from the user
+alarm_time = input("Enter the time of alarm to be set: HH:MM:SS\n")
+
+# Extract the hour, minute, second, and period from the alarm_time string
+alarm_hour = alarm_time[0:2]
+alarm_minute = alarm_time[3:5]
+alarm_seconds = alarm_time[6:8]
 alarm_period = alarm_time[9:11].upper()
-print("Setting up alarm..")
+
+# Print a message to indicate that the alarm is being set up
+print("Setting up alarm...")
+
+# Continuously check the current time against the alarm time until they match
 while True:
+    # Get the current time
     now = datetime.now()
+
+    # Extract the current hour, minute, second, and period from the current time
     current_hour = now.strftime("%I")
     current_minute = now.strftime("%M")
     current_seconds = now.strftime("%S")
     current_period = now.strftime("%p")
-    if(alarm_period==current_period):
-        if(alarm_hour==current_hour):
-            if(alarm_minute==current_minute):
-                if(alarm_seconds==current_seconds):
-                    print("Wake Up!")
-                    playsound('audio.mp3') ## download the alarm sound from link
+
+    # Check if the current period matches the alarm period
+    if(alarm_period == current_period):
+        # Check if the current hour matches the alarm hour
+        if(alarm_hour == current_hour):
+            # Check if the current minute matches the alarm minute
+            if(alarm_minute == current_minute):
+                # Check if the current second matches the alarm second
+                if(alarm_seconds == current_seconds):
+                    # Print a message to indicate that it's time to wake up
+                    print("Wake up!")
+
+                    # Play the alarm sound
+                    playsound('audio.mp3') # You can download the alarm sound from a link
+
+                    # Break out of the while loop
                     break
