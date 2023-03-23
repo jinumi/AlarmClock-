3b482c525c54beccd228bfbae968ea0bfff62139
Muhammad Umair Yaqub <umairyaqubofficial@gmail.com>
2023-03-23 13:08:22 +0500

    Modernization Update

--- Alarm Clock.py
+++ Alarm Clock.py
@@ -2,17 +2,25 @@
 # Made in P A K I S T A N
 
 # Import necessary modules
-from datetime import datetime   
+from datetime import datetime
+from time import sleep
 from playsound import playsound
 
 # Get the time of the alarm from the user
-alarm_time = input("Enter the time of alarm to be set: HH:MM:SS\n")
-
-# Extract the hour, minute, second, and period from the alarm_time string
-alarm_hour = alarm_time[0:2]
-alarm_minute = alarm_time[3:5]
-alarm_seconds = alarm_time[6:8]
-alarm_period = alarm_time[9:11].upper()
+while True:
+    alarm_time = input("Enter the time of alarm to be set: HH:MM:SS AM or PM\n")
+    try:
+        # Extract the hour, minute, second, and period from the alarm_time string
+        alarm_hour = int(alarm_time[0:2])
+        alarm_minute = int(alarm_time[3:5])
+        alarm_seconds = int(alarm_time[6:8])
+        alarm_period = alarm_time[9:11].upper()
+        if alarm_hour >= 0 and alarm_hour <= 12 and alarm_minute >= 0 and alarm_minute <= 59 and alarm_seconds >= 0 and alarm_seconds <= 59 and alarm_period in ['AM', 'PM']:
+            break
+        else:
+            print("Invalid time format, please try again.")
+    except:
+        print("Invalid input format, please try again.")
 
 # Print a message to indicate that the alarm is being set up
 print("Setting up alarm...")
@@ -23,24 +31,27 @@ while True:
     now = datetime.now()
 
     # Extract the current hour, minute, second, and period from the current time
-    current_hour = now.strftime("%I")
-    current_minute = now.strftime("%M")
-    current_seconds = now.strftime("%S")
+    current_hour = int(now.strftime("%I"))
+    current_minute = int(now.strftime("%M"))
+    current_seconds = int(now.strftime("%S"))
     current_period = now.strftime("%p")
 
     # Check if the current period matches the alarm period
-    if(alarm_period == current_period):
+    if alarm_period == current_period:
         # Check if the current hour matches the alarm hour
-        if(alarm_hour == current_hour):
+        if alarm_hour == current_hour:
             # Check if the current minute matches the alarm minute
-            if(alarm_minute == current_minute):
+            if alarm_minute == current_minute:
                 # Check if the current second matches the alarm second
-                if(alarm_seconds == current_seconds):
+                if alarm_seconds == current_seconds:
                     # Print a message to indicate that it's time to wake up
                     print("Wake up!")
 
                     # Play the alarm sound
-                    playsound('audio.mp3') # You can download the alarm sound from a link
+                    # You can download the alarm sound from a link
+                    playsound('audio.mp3')
 
                     # Break out of the while loop
                     break
+    # Add a delay to the while loop to avoid the program from running continuously and using up system resources.
+    sleep(1)
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# AlarmClock-
+# Alarm Clock
+
+This is a simple alarm clock script written in Python that allows the user to set a specific time for the alarm to go off. When the alarm time is reached, the script will play an audio file to wake up the user.
+
+## Getting Started
+
+### Prerequisites
+
+- Python 3.x
+- playsound module
+
+### Installing
+
+1. Clone the repository to your local machine.
+2. Install the playsound module by running the following command in your terminal:
+
+# pip install playsound
+
+### Usage
+
+1. Open the terminal and navigate to the directory where the script is located.
+2. Run the script by executing the following command:
+
+# python alarm_clock.py
+
+3. Enter the time of the alarm in the following format: HH:MM:SS AM/PM
+4. Wait for the alarm to go off at the specified time.
+
+### Example
+
+Enter the time of alarm to be set: 07:30:00 AM
+Setting up alarm...
+Wake up!
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details.
+
+## Acknowledgments
+
+- The [playsound module](https://pypi.org/project/playsound/) for playing audio files in Python.
